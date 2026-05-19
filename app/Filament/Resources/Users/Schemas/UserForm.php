c68c9b4379b4b970d1390e6444ca2fde13599bcb
Zaine <kyawkokozin223@gmail.com>
2026-05-19 13:19:18 +0630

    fix : route

--- app/Filament/Resources/Users/Schemas/UserForm.php
+++ app/Filament/Resources/Users/Schemas/UserForm.php
@@ -20,7 +20,6 @@ class UserForm
                     ->label('Email address')
                     ->email()
                     ->required(),
-                DateTimePicker::make('email_verified_at'),
                 TextInput::make('password')
                     ->password()
                     ->required(),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Enums\UserRole;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -18,10 +19,20 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
+        // Create Admin account
         User::factory()->create([
             'name' => 'Admin',
             'email' => 'admin@gmail.com',
             'password' => Hash::make('admin111'),
+            'role' => UserRole::Admin,
+        ]);
+
+        // Create EZGO account
+        User::factory()->create([
+            'name' => 'EZGO',
+            'email' => 'ezgo@gmail.com',
+            'password' => Hash::make('ezgo111'),
+            'role' => UserRole::Ezgo,
         ]);
     }
 }
